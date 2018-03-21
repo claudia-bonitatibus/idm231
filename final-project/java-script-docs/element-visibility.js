7ed92b8c5ca220ac77b6c89b9cc9c262d614ccc9
claudia-bonitatibus <32339275+claudia-bonitatibus@users.noreply.github.com>
2018-03-21 13:51:31 -0400

    Update element-visibility.js

--- final-project/java-script-docs/element-visibility.js
+++ final-project/java-script-docs/element-visibility.js
@@ -1,247 +1,108 @@
-var selectedIcon1 = document.getElementById("creature1Icon");
-var selectedIcon2 = document.getElementById("creature2Icon");
-var selectedIcon3 = document.getElementById("creature3Icon");
-var selectedIcon4 = document.getElementById("creature4Icon");
-var selectedIcon5 = document.getElementById("creature5Icon");
-var selectedIcon6 = document.getElementById("creature6Icon");
-var selectedIcon7 = document.getElementById("creature7Icon");
-var selectedIcon8 = document.getElementById("creature8Icon");
-var selectedIcon9 = document.getElementById("creature9Icon");
-var selectedIcon10 = document.getElementById("creature10Icon");
-var selectedIcon11 = document.getElementById("creature11Icon");
-var selectedIcon12 = document.getElementById("creature12Icon");
+var gridIcon = [
+document.getElementById("creature1Icon"),
+document.getElementById("creature2Icon"),
+document.getElementById("creature3Icon"),
+document.getElementById("creature4Icon"),
+document.getElementById("creature5Icon"),
+document.getElementById("creature6Icon"),
+document.getElementById("creature7Icon"),
+document.getElementById("creature8Icon"),
+document.getElementById("creature9Icon"),
+document.getElementById("creature10Icon"),
+document.getElementById("creature11Icon"),
+document.getElementById("creature12Icon"),]
 
-var creature1 = document.getElementById("creature1");
-var creature2 = document.getElementById("creature2");
-var creature3 = document.getElementById("creature3");
-var creature4 = document.getElementById("creature4");
-var creature5 = document.getElementById("creature5");
-var creature6 = document.getElementById("creature6");
-var creature7 = document.getElementById("creature7");
-var creature8 = document.getElementById("creature8");
-var creature9 = document.getElementById("creature9");
-var creature10 = document.getElementById("creature10");
-var creature11 = document.getElementById("creature11");
-var creature12 = document.getElementById("creature12");
+var descriptionCard = [
+document.getElementById("creature1"),
+document.getElementById("creature2"),
+document.getElementById("creature3"),
+document.getElementById("creature4"),
+document.getElementById("creature5"),
+document.getElementById("creature6"),
+document.getElementById("creature7"),
+document.getElementById("creature8"),
+document.getElementById("creature9"),
+document.getElementById("creature10"),
+document.getElementById("creature11"),
+document.getElementById("creature12"),]
+
+var deselect= 0;
+var clicked = 0;
 
 var width = window.innerWidth;
 
-function selectAirElement(){
-	console.log("AIR!");
+function showHidden(){
+	for (deselect= 0; deselect< descriptionCard.length; deselect++) {
+		    if (descriptionCard[deselect].classList == "showDescription" && descriptionCard[i] != descriptionCard[clicked]){
+		    	descriptionCard[deselect].classList = "hide";
+		    }
+	}
+
+	if(descriptionCard[clicked].classList == "hide"){
+		descriptionCard[clicked].classList = "showDescription";
+	}
+
+	else {
+		descriptionCard[clicked].classList = "hide";
+	}
 }
 
-function selectWaterElement(){
-	console.log("WATER!");
-}
-
-function selectFireElement(){
-	console.log("FIRE!");
-}
-
-function selectEarthElement(){
-	console.log("EARTH!");
-}
-
-
-selectedIcon1.addEventListener('click', (function (){
-	if (creature1.style.display === "none") {
-	    if(width >= 870){
-	    	creature1.style.display = "grid";
-	    }
-	    else{
-	    	creature1.style.display = "block"; //DOES NOT DIPLAY CORRECTLY
-	    	creature1.style.textAlign = "center";
-	    }
-	} 
-
-	else {
-		creature1.style.display = "none";
-	}
+gridIcon[0].addEventListener('click', (function(){
+	clicked = 0;
+	showHidden();
 }),false);
 
-selectedIcon2.addEventListener('click', (function (){
-	if (creature2.style.display === "none") {
-	    if(width >= 870){
-	    	creature2.style.display = "grid";
-	    }
-	    else{
-	    	creature2.style.display = "block";
-	    	creature2.style.textAlign = "center";
-	    }
-	} 
-
-	else {
-		creature2.style.display = "none";
-	}
-}),false);
-	
-selectedIcon3.addEventListener('click',(function (){
-	if (creature3.style.display === "none") {
-	    if(width >= 870){
-	    	creature3.style.display = "grid";
-	    }
-	    else{
-	    	creature3.style.display = "block";
-	    	creature3.style.textAlign = "center";
-	    }
-	} 
-
-	else {
-		creature3.style.display = "none";
-	}
+gridIcon[1].addEventListener('click',  (function(){
+	clicked = 1;
+	showHidden();
 }),false);
 
-selectedIcon4.addEventListener('click', (function (){
-	if (creature4.style.display === "none") {
-	    if(width >= 870){
-	    	creature4.style.display = "grid";
-	    }
-	    else{
-	    	creature4.style.display = "block";
-	    	creature4.style.textAlign = "center";
-	    }
-	} 
-
-	else {
-		creature4.style.display = "none";
-	}
-}),false);
-	
-selectedIcon5.addEventListener('click', (function (){
-	if (creature5.style.display === "none") {
-	    if(width >= 870){
-	    	creature5.style.display = "grid";
-	    }
-	    else{
-	    	creature5.style.display = "block";
-	    	creature5.style.textAlign = "center";
-	    }
-	} 
-
-	else {
-		creature5.style.display = "none";
-	}
+gridIcon[2].addEventListener('click',  (function(){
+	clicked = 2;
+	showHidden();
 }),false);
 
-selectedIcon6.addEventListener('click', (function (){
-	if (creature6.style.display === "none") {
-	    if(width >= 870){
-	    	creature6.style.display = "grid";
-	    }
-	    else{
-	    	creature6.style.display = "block";
-	    	creature6.style.textAlign = "center";
-	    }
-	} 
-
-	else {
-		creature6.style.display = "none";
-	}
-}),false);
-	
-selectedIcon7.addEventListener('click', (function (){
-	if (creature7.style.display === "none") {
-	    if(width >= 870){
-	    	creature7.style.display = "grid";
-	    }
-	    else{
-	    	creature7.style.display = "block";
-	    	creature7.style.textAlign = "center";
-	    }
-	} 
-
-	else {
-		creature7.style.display = "none";
-	}
-}),false);
-	
-selectedIcon8.addEventListener('click', (function (){
-	if (creature8.style.display === "none") {
-	    if(width >= 870){
-	    	creature8.style.display = "grid";
-	    }
-	    else{
-	    	creature8.style.display = "block";
-	    	creature8.style.textAlign = "center";
-	    }
-	} 
-
-	else {
-		creature8.style.display = "none";
-	}
-}),false);
-	
-selectedIcon9.addEventListener('click', (function (){
-	if (creature9.style.display === "none") {
-	    if(width >= 870){
-	    	creature9.style.display = "grid";
-	    }
-	    else{
-	    	creature9.style.display = "block";
-	    	creature9.style.textAlign = "center";
-	    }
-	} 
-
-	else {
-		creature9.style.display = "none";
-	}
-}),false);
-	
-selectedIcon10.addEventListener('click',(function (){
-	if (creature10.style.display === "none") {
-	    if(width >= 870){
-	    	creature10.style.display = "grid";
-	    }
-	    else{
-	    	creature10.style.display = "block";
-	    	creature10.style.textAlign = "center";
-	    }
-	} 
-
-	else {
-		creature10.style.display = "none";
-	}
-}),false);
-	
-selectedIcon11.addEventListener('click', (function (){
-	if (creature11.style.display === "none") {
-	    if(width >= 870){
-	    	creature11.style.display = "grid";
-	    }
-	    else{
-	    	creature11.style.display = "block";
-	    	creature11.style.textAlign = "center";
-	    }
-	} 
-
-	else {
-		creature11.style.display = "none";
-	}
-}),false);
-	
-selectedIcon12.addEventListener('click', (function (){
-	if (creature12.style.display === "none") {
-	    if(width >= 870){
-	    	creature12.style.display = "grid";
-	    }
-	    else{
-	    	creature12.style.display = "block";
-	    	creature12.style.textAlign = "center";
-	    }
-	} 
-
-	else {
-		creature12.style.display = "none";
-	}
+gridIcon[3].addEventListener('click',  (function(){
+	clicked = 3;
+	showHidden();
 }),false);
 
+gridIcon[4].addEventListener('click',  (function(){
+	clicked = 4;
+	showHidden();
+}),false);
 
+gridIcon[5].addEventListener('click',  (function(){
+	clicked = 5;
+	showHidden();
+}),false);
 
+gridIcon[6].addEventListener('click',  (function(){
+	clicked = 6;
+	showHidden();	
+}),false);
 
+gridIcon[7].addEventListener('click',  (function(){
+	clicked = 7;
+	showHidden();
+}),false);
 
+gridIcon[8].addEventListener('click',  (function(){
+	clicked = 8;
+	showHidden();
+}),false);
 
+gridIcon[9].addEventListener('click',  (function(){
+	clicked = 9;
+	showHidden();
+}),false);
 
+gridIcon[10].addEventListener('click',  (function(){
+	clicked = 10;
+	showHidden();
+}),false);
 
-
-
-
+gridIcon[11].addEventListener('click',  (function(){
+	clicked = 11;
+	showHidden();
+}),false);
